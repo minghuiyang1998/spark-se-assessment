617f083ce046bb75a646e5a2828b5f1316c57be5
Yang Minghui <minghuiyang1998@gmail.com>
2022-09-23 15:32:25 -0400

    fix

--- launch.sh
+++ launch.sh
@@ -5,5 +5,5 @@ export APP_SETTINGS="project.server.config.DevelopmentConfig"
 flask db init
 flask db migrate
 flask db upgrade
-flask run --host=0.0.0.0 --port=5000
+flask run --host=https://minghui1998-spark-heroku.herokuapp.com/  --port=5000
 # flask run
